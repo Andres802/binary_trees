c63d8eb1b6bf2f07dc50082cd6f5bf20b7ccca24
Andres802 <reachandres@gmail.com>
2020-11-27 04:48:16 +0000

    betty fx

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - function that measures the balance factor of a binary tree
+ * binary_tree_balance - function that measures the balance factor of a binary 
+ * tree
  * @tree: is a pointer to the root node of the tree to measure the height.
  * Return: the height of the tree
  */
